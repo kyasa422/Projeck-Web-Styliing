7fe2ad5f45323555a0dabd8a2d970528f2a16b37
Eugene702 <eugenefeilianputrarangga@gmail.com>
2022-08-30 20:50:10 +0700

    Kelar bos full tailwindcss awas jangan bingung

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'silkscreen': ['Silkscreen', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
